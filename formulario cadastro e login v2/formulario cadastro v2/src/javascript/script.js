94affac1c54833014ea68db5cd8f055c1cd25870
igorgsantoss <igorgabriel1432@gmail.com>
2025-08-25 11:38:57 -0300

    script.js

--- formulario cadastro e login v2/formulario cadastro v2/src/javascript/script.js
+++ formulario cadastro e login v2/formulario cadastro v2/src/javascript/script.js
@@ -8,16 +8,26 @@ passwordIcons.forEach(icon => {
         this.classList.toggle('fa-eye');
     })
 })
-
-
 document.getElementById('cpf').addEventListener('input', function(e) {
-    let value = e.target.value.replace(/\D/g, ''); // Remove tudo que não for número
-    if (value.length > 11) value = value.slice(0, 11); // Limita a 11 dígitos
+    let value = e.target.value.replace(/\D/g, ''); // Remove tudo que não é dígito
+    if (value.length > 11) value = value.slice(0, 11);
     value = value.replace(/(\d{3})(\d)/, '$1.$2');
     value = value.replace(/(\d{3})(\d)/, '$1.$2');
     value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
     e.target.value = value;
 });
+document.getElementById('form').addEventListener('submit', function(e) {
 
 
+const senha = document.getElementById('password').value;
+const csenha = document.getElementById('confirm_password').value;
+
+
+  if (senha !== csenha) {
+    alert ("Senhas diferentes!");
+    e.preventDefault();
+    return;
+  }
+}
+);
 
