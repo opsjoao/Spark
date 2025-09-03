010a457313ac6bc37e36be193982f07120f5314f
opsjoao <joaogmpifsp@gmail.com>
2025-09-02 21:51:45 -0300

    Delete formulario cadastro e login directory

--- formulario cadastro e login/formulario login/logout.php
+++
@@ -1,7 +0,0 @@
-<!-- filepath: c:\Users\Aluno\Documents\formulario cadastro eventos\logout.php -->
-<?php
-session_start();
-session_destroy();
-header("Location: formulario-login.html");
-exit();
-?>
