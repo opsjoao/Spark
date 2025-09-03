2df5c45422f8114864956e508661365742c82ea0
opsjoao <joaogmpifsp@gmail.com>
2025-09-02 21:55:04 -0300

    Delete tela principal directory

--- tela principal/telaprincipal.js
+++
@@ -1,7 +0,0 @@
-// Exemplo de interação simples
-document.querySelectorAll(".bottombar a").forEach(link => {
-  link.addEventListener("click", e => {
-    document.querySelectorAll(".bottombar a").forEach(a => a.classList.remove("active"));
-    e.currentTarget.classList.add("active");
-  });
-});
