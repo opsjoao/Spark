83df8e0570b09170c798f4ddc17d4b7b8625392f
igorgsantoss <igorgabriel1432@gmail.com>
2025-09-17 16:38:18 -0300

    Add files via upload

---
+++ formulario-cadastro-login/conexao.php
@@ -0,0 +1,15 @@
+<?php
+// Detalhes de conexão com o banco de dados
+$servername = "localhost";
+$username = "root"; 
+$password = "1234"; 
+$dbname = "Spark";
+
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica a conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+?>
