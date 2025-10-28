db06a075b9e0c287c99fc0ddf2a1d1ae65ddf042
Gustavo Kobayashi <152326594+kobayashigus@users.noreply.github.com>
2025-10-28 09:40:37 -0300

    Update telaprincipal.php

--- tela-principal/telaprincipal.php
+++ tela-principal/telaprincipal.php
@@ -46,6 +46,7 @@ $resultado_feed = $conexao->query($sql_feed);
     <title>Spark</title>
     <link rel="stylesheet" href="telaprincipal.css">
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+    <link href="">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 <body>
